3b9ba813483a16e9f4334a79c7b653976c411ff9
matecamilion <mateocamilion@gmail.com>
2023-07-08 12:05:43 -0300

    Primer commit proyecto final

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Selecciona una opcion: ");
+	        System.out.println("1. Ver stock");
+	        System.out.println("2. Agregar stock");
+	        System.out.println("3. Stock vendido");
+
+	        int opcion = scanner.nextInt();
+
+	      
+	
+
+	}
+}
